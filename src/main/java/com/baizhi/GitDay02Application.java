0849159bcc99536d50f2e924d1d8fc496b8c9925
zhangk312 <55437095+zhangk312@users.noreply.github.com>
2019-09-18 19:28:58 +0800

    Update GitDay02Application.java

--- src/main/java/com/baizhi/GitDay02Application.java
+++ src/main/java/com/baizhi/GitDay02Application.java
@@ -9,6 +9,7 @@ public class GitDay02Application {
     public static void main(String[] args) {
         SpringApplication.run(GitDay02Application.class, args);
         System.out.println("123");
+        System.out.println("456");
     }
 
 }
