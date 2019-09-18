11b406c1a3cb97a6804ef55b9a1d22072e65c25b
zk <2856547681@qq.com>
2019-09-18 19:52:40 +0800

    2.0

--- src/main/java/com/baizhi/GitDay02Application.java
+++ src/main/java/com/baizhi/GitDay02Application.java
@@ -11,6 +11,7 @@ public class GitDay02Application {
         System.out.println("123");
         System.out.println("456");
         System.out.println("789");
+        System.out.println(123);
     }
 
 }
